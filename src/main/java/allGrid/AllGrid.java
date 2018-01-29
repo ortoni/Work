edf561ffad364ec2a66834eb5d6fd166f0381d2c
TestLeaf <TestLeaf@DESKTOP-23TDLJK>
2018-01-29 17:19:02 +0530

    gen

--- src/main/java/allGrid/AllGrid.java
+++ src/main/java/allGrid/AllGrid.java
@@ -11,11 +11,11 @@ import org.testng.annotations.Test;
 public class AllGrid {
 	@Test
 	public void gridEdge() throws MalformedURLException{
-		DesiredCapabilities dc = new DesiredCapabilities();
-		dc.setBrowserName("internet explorer");
-		dc.setPlatform(Platform.WINDOWS);		
+		DesiredCapabilities caps = DesiredCapabilities.safari();
+		caps.setCapability("platform", "macOS 10.12");
+		caps.setCapability("version", "11.0");		
 		RemoteWebDriver driver = new RemoteWebDriver(
-				new URL("http://192.168.1.32:4444/wd/hub"), dc);
+				new URL("http://192.168.121.255:4444/wd/hub"), caps);
 		driver.get("https://www.google.com");
 	}
 	
